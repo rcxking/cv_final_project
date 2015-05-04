17b79e941689bc424e906f98d7d969e1565d3ac9
Bryant Pong <rcxking@gmail.com>
2015-05-04 19:33:02 -0400

    Began Neural Network Training module

---
+++ ros_workspace/src/regions/src/nntrain.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+'''
+nntrain.py - This script trains a neural network given a set of images
+to help identify regions of interest.
+
+Bryant Pong / Micah Corah      
+CSCI-4962
+5/4/15
+
+Last Updated: Bryant Pong: 5/4/15 - 5:20 PM
+'''
+
+# Python Imports:
+import numpy as np
+import cv2
+from sklearn.neural_network import BernoulliRBM
+
+'''
+This function helps generate transformations  
+'''
+
+# Training function:
+def train():
+	
+	pass
+
+# Training Runner
+if __name__ == "__main__":
+	train()
