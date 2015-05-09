ab6c8d34f3f132421d5f194b09cd9d441268ddc4
Bryant Pong <rcxking@gmail.com>
2015-05-08 23:20:35 -0400

    Adding logistic classifier to project

--- ros_workspace/src/regions/src/nntrain.py
+++ ros_workspace/src/regions/src/nntrain.py
@@ -8,22 +8,55 @@ Bryant Pong / Micah Corah
 CSCI-4962
 5/4/15
 
-Last Updated: Bryant Pong: 5/4/15 - 5:20 PM
+Last Updated: Bryant Pong: 5/8/15 - 5:21 PM
 '''
 
 # Python Imports:
 import numpy as np
 import cv2
 from sklearn.neural_network import BernoulliRBM
+from sklearn.linear_model import LogisticRegression # A logistic classifier
+import cPickle as pickle # cPickle is faster for Python 2.X 
+import os # listdir() function
 
 '''
 This function helps generate transformations  
 '''
 
+'''
+This function loads Pickle data containing images and the marked regions
+of interest:         
+'''
+def loadData(imgFolder):
+
+	# Uncomment when running real neural network training:
+	#return [pickle.load(open(imgFolder+"/"+str(i), "rb")) for i in sorted(os.listdir(imgFolder))]
+	return [pickle.load(open(imgFolder+"/20150423_152339.dat", "rb"))] 
+
 # Training function:
 def train():
 	
-	pass
+	# The neural network object:
+	neuralNetwork = BernoulliRBM()   
+
+	# Load the training data:
+	trainingData = loadData("../../../../data/pickle")
+	
+	# Next iterate through each image in the dataset and perform transformations on the image:
+	for i in xrange(len(trainingData)):
+		nextTrainingSet = trainingData[i]
+
+		for j in xrange(len(nextTrainingSet)):
+			nextImage = nextTrainingSet[j]
+
+			nextImageData = nextImage[0]
+			nextImageFeatures = nextImage[1]
+			
+			print("nextImageData: " + str(nextImageData))
+			print("nextImageFeatures: " + str(nextImageFeatures))				
+		
+			
+			
 
 # Training Runner
 if __name__ == "__main__":
